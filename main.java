d9c0c933243ddedf29d9c296e66e764faf0389f2
ngaytho1210 <49218233+ngaytho1210@users.noreply.github.com>
2019-04-03 10:06:21 +0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,23 @@
+package baithi;
+
+public class Main {
+    public static void main(String[] args){
+        baithi.Hotel hotel = new baithi.Hotel();
+        hotel.EnterHotel();
+        hotel.InforHotel();
+
+        baithi.Hotel hotel1 = new baithi.Hotel();
+        hotel.EnterHotel();
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
